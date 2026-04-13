e27e021d1b6bf989c4c20ab3f3eabbca1af955e7
dwells66 <dwells66@students.claremontmckenna.edu>
2026-04-13 01:30:59 -0700

    test2

--- load_tweets_parallel.sh
+++ load_tweets_parallel.sh
@@ -5,14 +5,17 @@ files=$(find data/*)
 echo '================================================================================'
 echo 'load pg_denormalized'
 echo '================================================================================'
+#time sh -c 'find data/* | parallel ./load_denormalized.sh'
 # FIXME: implement this with GNU parallel
 
 echo '================================================================================'
 echo 'load pg_normalized'
 echo '================================================================================'
-# FIXME: implement this with GNU parallel
+#time sh -c 'find data/* | parallel ./load_normalized.sh'
+#FIXME: implement this with GNU parallel
 
 echo '================================================================================'
 echo 'load pg_normalized_batch'
 echo '================================================================================'
+time sh -c 'find data/* | parallel ./load_normalized_batch.sh'
 # FIXME: implement this with GNU parallel
